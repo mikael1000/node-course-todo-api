c1ae19ba1c9251fe63c1ee9afd8491ff52091559
mikael1000 <mikael.johansson.1000@live.se>
2018-10-25 02:38:43 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -50,6 +50,30 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    // Remove todo by id
+        // success
+            // if no doc, send 404
+            // if doc, send doc back with 200
+        // error
+            // 400 with empty body
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send()
+        }
+
+        res.send({todo})
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`)
 })
