659a187079c87a403bcfe96b2d8c53b3bc8d6b02
mikael1000 <mikael.johansson.1000@live.se>
2018-10-30 19:55:29 +0100

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -111,7 +111,7 @@ app.post('/users', (req, res) => {
     })
 })
 
-app.get('/users/me',authenticate, (req, res) => {
+app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user)
 })
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,23 +4,11 @@ const { ObjectID } = require('mongodb')
 
 const { app } = require('./../server')
 const { Todo } = require('./../models/todo')
+const { User } = require('./../models/user')
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed')
 
-const todos = [{
-    _id: new ObjectID,
-    text: 'First test todo'
-},
-{
-    _id: new ObjectID,
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-}]
-
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        return Todo.insertMany(todos)
-    }).then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -88,7 +76,6 @@ describe('GET /todos/:id', () => {
     })
 
     it('should return 404 if todo not found', (done) => {
-        // make sure you get a 404 back
         const hexId = new ObjectID().toHexString();
 
         request(app)
@@ -99,7 +86,6 @@ describe('GET /todos/:id', () => {
     })
 
     it('should return 404 for non-object ids', (done) => {
-        // /todos/123
         request(app)
             .get(`/todos/1234`)
             .expect(404)
@@ -183,13 +169,82 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text)
                 expect(res.body.todo.completed).toBe(false)
-                //expect(res.body.todo.completedAt).toBe(null)
                 expect(res.body.todo.completedAt).toNotExist()
             })
             .end(done)
-        // grab id of second todo item
-        // update text, set completed to false
-        // 200
-        // text is changed, completed false, completedAt is null .toNotExist
+    })
+})
+
+describe('GET /users/me', () => {
+    it('should return user if authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body._id).toBe(users[0]._id.toHexString())
+                expect(res.body.email).toBe(users[0].email)
+            })
+            .end(done)
+    })
+
+    it('should return a 401 if not authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect((res) => {
+                expect(res.body).toEqual({})
+            })
+            .end(done)
+    })
+})
+
+describe('POST /users/', () => {
+    it('should create a user', (done) => {
+        const email = 'example@example.com'
+        const password = '123mnb!'
+
+        request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist()
+                expect(res.body._id).toExist()
+                expect(res.body.email).toBe(email)
+            })
+            .end((err) => {
+                if(err) {
+                    return done(err)
+                }
+
+                User.findOne({email}).then((user) => {
+                    expect(user).toExist()
+                    expect(user.password).toNotBe(password)
+                    done()
+                })
+            })
+    })
+
+    it('should return validation error if request invalid', (done) => {
+        const email = 'example'
+        const password = '123'
+
+        request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(400)
+            .end(done)
+    })
+
+    it('should not create user if email in use', (done) => {
+        const email = users[0].email // 'andrew@example.com'
+        const password = '123abc!'
+
+        request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(400)
+            .end(done)
     })
 })
